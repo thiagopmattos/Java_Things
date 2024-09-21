4121570b7253f5c502be0ed9f79bd6997aeedcf7
thiago <thiago.pmattos@hotmail.com>
2024-09-21 18:11:11 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+    Pizzaria doisImaos = new Pizzaria();
+    doisImaos.diaDeHoje();
+
+    }
+
+
+}
+
+// uma pizzaria que dia impar ela serve um sabor e dia par outro, a cada dez dias muda os dois sabores.
